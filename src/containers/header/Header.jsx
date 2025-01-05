2edffff52f93bfb515d756d78ebd6713243eb543
Behnam <behnam.sep@gmail.com>
2025-01-05 20:06:04 +0100

    finish Hero section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,16 +1,36 @@
 import React from "react";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 import "./header.css";
-const Header = () => {
-  return (
-    <div className="gp3__header section">
-      <div className="gp3__header-content">
-        <h1 className="gradient__text">
-          Let's Build Something amazing with GPT-3 OpenAI
-        </h1>
-        
+
+const Header = () => (
+  <div className="gpt5__header section__padding" id="home">
+    <div className="gpt5__header-content">
+      <h1 className="gradient__text">
+        Create Something Amazing with OpenAI's GPT-5!
+      </h1>
+      <p>
+        Still, any form of assistance for travel seemed unsatisfying. Not all
+        thoughts brought the blessing of action. Joyful changes and lively
+        connections marked the path of indulgence. Over the years, we permitted
+        what was requested to keep things in order.
+      </p>
+
+      <div className="gpt5__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt5__header-content__people">
+        <img src={people} />
+        <p>Over 2,00 people requested access within the past 24 hours.</p>
       </div>
     </div>
-  );
-};
+
+    <div className="gpt5__header-image">
+      <img src={ai} />
+    </div>
+  </div>
+);
 
 export default Header;
