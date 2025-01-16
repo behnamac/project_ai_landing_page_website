1650efe46659ad1a22e2c0f2510efb2234f0d0df
Behnam <behnam.sep@gmail.com>
2025-01-16 17:39:06 +0100

    refactor navbar list

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt5">What is GPT5?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Features</a></p>
+    <p><a href="#blog">Blog</a></p>
+  </>
+)
+
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
@@ -13,11 +23,7 @@ const Navbar = () => {
           <img src={logo} />
         </div>
         <div className="gpt5__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt5">What is GPT5?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Features</a></p>
-          <p><a href="#blog">Blog</a></p>
+         <Menu />
         </div>
       </div>
       <div className="gpt5__navbar-sign">
@@ -31,11 +37,7 @@ const Navbar = () => {
         {toggleMenu && (
         <div className="gpt5__navbar-menu_container scale-up-center">
           <div className="gpt5__navbar-menu_container-links">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt5">What is GPT5?</a></p>
-            <p><a href="#possibility">Open AI</a></p>
-            <p><a href="#features">Case Studies</a></p>
-            <p><a href="#blog">Library</a></p>
+         <Menu />
           </div>
           <div className="gpt5__navbar-menu_container-links-sign">
             <p>Sign in</p>
